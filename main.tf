ecf9218f88f3f6006e2c4a8d6a449d43ab8c5402
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2023-09-26 13:16:17 +0200

    fix: deprecated retention policy

--- main.tf
+++ main.tf
@@ -26,9 +26,9 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting" {
       # Learn more information on the deprecation [in the Azure documentation](https://aka.ms/diagnostic_settings_log_retention).
       # https://github.com/hashicorp/terraform-provider-azurerm/pull/23260
       # https://github.com/hashicorp/terraform-provider-azurerm/issues/23051
-      retention_policy {
-        enabled = true
-      }
+      # retention_policy {
+      #   enabled = true
+      # }
     }
   }
 
